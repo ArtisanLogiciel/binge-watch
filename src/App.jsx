25b88972ae5dcad7ba80e6ee5a6f34b61e06c642
butch <au.bouchard@gmail.com>
2022-09-28 21:59:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+      <div>
+          <p className={`text-white bg-violet-400`}>
+              Let's go binge watch !
+          </p>
+      </div>
+  )
+}
+
+export default App
