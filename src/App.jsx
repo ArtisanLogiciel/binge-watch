860346560268b7d8f564c072689f2fbb2fc7cd6f
antonin michel <a.copiercoller@gmail.com>
2022-09-29 17:18:34 +0200

    personnalisation de l'interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from 'react-dom';
-
+import PopCorn from './assets/popcorn.svg'
 function App() {
   return (
     <>
-      <div>
-        <p className={`text-white bg-violet-400`}>Binge Watch !</p>
+      <div className="bg-gray-900 h-screen">
+        <img src={PopCorn} alt="logo" className="mx-auto" />
+        <h1 className={`text-white text-4xl text-center`}>BingeWatch</h1>
       </div>
     </>
   );
 }
-
 export default App;
