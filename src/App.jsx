b97f1f5ae09d0caeb11dc44477ff6689286ab279
butch <au.bouchard@gmail.com>
2022-09-30 17:26:16 +0200

    environment ok for vite

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './views/Home'
 import Error404 from './views/Error404'
 import NavBar from "./components/NavBar";
+import {auth} from "../firebase-config"
+
+import {
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    onAuthStateChanged
+} from "firebase/auth"
+
+
 function App() {
+    
+    // test
+    const signUp = (email, pwd) => createUserWithEmailAndPassword(auth, email, pwd)
+    
+    
   return (
     <>
       <NavBar />
@@ -13,7 +27,11 @@ function App() {
           <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
+        {signUp ? <div>
+            {JSON.stringify(signUp("email", "pwd"))}
+        </div> : null}
     </>
   );
 }
+
 export default App;
