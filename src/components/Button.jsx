28877415806fe3b03952bd20c05491d70674ef69
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2022-10-04 17:16:58 +0200

    Seacr input + logging icon + logo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
-export default function Button({onClick, className, children}) {
-    
-    return (
-        <div className={`bg-binge p-4 rounded uppercase ${className}`} role={'Button'} onClick={onClick}>
-            {children}
-        </div>
-    )
+export default function Button({ onClick, className, children }) {
+  return (
+    <div
+      className={`bg-binge p-4 rounded uppercase ${className}`}
+      role={"Button"}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
 }
