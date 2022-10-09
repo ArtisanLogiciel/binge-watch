0266f4cb02816df610ca9ecd239b577321a0e30b
butch <au.bouchard@gmail.com>
2022-10-09 15:20:42 +0200

    button OK

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 
+/**
+ * Default style :
+ * bg-binge p-4 rounded-md uppercase cursor-pointer transition-all duration-400
+ * Style can be overridden or extended
+ *
+ * @param onClick
+ * @param className
+ * @param children
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function Button({ onClick, className, children }) {
   return (
     <div
-      className={`bg-binge p-4 rounded uppercase ${className}`}
+      className={`bg-binge flex items-center sm:p-4 md:p-4 lg:p-4 rounded-md
+      uppercase text-white text-xs sm:text-base md:text-lg sm:tracking-wider leading-4.5
+       cursor-pointer transition-all duration-400 ${className}`}
       role={"Button"}
       onClick={onClick}
     >
-      {children}
+      <span className={`inline align-text-top h-[15px] md:h-4`}>
+        {children}
+      </span>
     </div>
   );
 }
